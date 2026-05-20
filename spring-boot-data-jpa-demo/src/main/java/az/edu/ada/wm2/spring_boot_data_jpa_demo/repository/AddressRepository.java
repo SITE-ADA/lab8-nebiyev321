bc60aa2a1586b9e0fa4a932ea71f35af052a684b
Vidadi <vnabiyev13119@ada.edu.az>
2026-05-20 20:56:07 +0400

    deleted folder

--- spring-boot-data-jpa-demo/src/main/java/az/edu/ada/wm2/spring_boot_data_jpa_demo/repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package az.edu.ada.wm2.spring_boot_data_jpa_demo.repository;
-
-import az.edu.ada.wm2.spring_boot_data_jpa_demo.model.entity.AddressEntity;
-import lombok.NonNull;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<@NonNull AddressEntity, @NonNull Long> {
-}
